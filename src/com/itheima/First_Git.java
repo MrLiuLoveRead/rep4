36a6094965e08c837e0ff0eb24423d70c7e5f5d4
MrLiu <823616203@qq.com>
2019-07-24 14:05:19 +0800

    添加了i变量

--- src/com/itheima/First_Git.java
+++ src/com/itheima/First_Git.java
@@ -6,6 +6,7 @@ public class First_Git {
         System.out.println("hello Git");
         System.out.println("hello Git");
         System.out.println("hello Git");
+        //添加了i变量;
     }
 
     public void test(){
@@ -13,5 +14,7 @@ public class First_Git {
         System.out.println("next ....");
         System.out.println("next ....");
         System.out.println("next ....");
+        int i = 0;
+        i+=3;
     }
 }
