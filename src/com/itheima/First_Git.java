3bdf5fc21d18233022b8cca0aac9d11046e2b0b1
MrLiu <823616203@qq.com>
2019-07-24 12:38:54 +0800

    把工程添加到本地仓库

---
+++ src/com/itheima/First_Git.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class First_Git {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+    }
+}
